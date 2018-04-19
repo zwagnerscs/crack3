35c44de1bff6c338fa8151babf427ecd966938d7
Barry Brown <profbbrown@gmail.com>
2018-04-19 10:53:45 -0700

    Removed references to -l ssl

--- md5.h
+++ md5.h
@@ -1,6 +1,6 @@
 /*
  * Create an MD5 hex digest from a string.
  * 33-byte string is malloc'd by this function. The caller must free it.
- * When compiling, link to:  -l ssl -l crypto
+ * When compiling, link to:  -l crypto
  */
 char *md5(const char *str, int length);
