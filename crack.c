cd6ff79cf4dc8101517f39125eaa4e389d8c84c3
Barry Brown <profbbrown@gmail.com>
2018-04-19 10:59:52 -0700

    Modified crack.c for assignment

--- crack.c
+++ crack.c
@@ -7,6 +7,11 @@
 const int PASS_LEN=50;        // Maximum any password can be
 const int HASH_LEN=33;        // Length of MD5 hash strings
 
+struct entry 
+{
+    // FILL THIS IN
+};
+
 // Given a hash and a plaintext guess, return 1 if
 // the hash of the guess matches the given hash.
 // That is, return 1 if the guess is correct.
@@ -21,20 +26,14 @@ int tryguess(char *hash, char *guess)
     return 0;
 }
 
-// TODO
-// Read in the hash file and return the array of strings.
-char **read_hashes(char *filename)
-{
-    return NULL;
-}
-
 
 // TODO
 // Read in the dictionary file and return the data structure.
 // Each entry should contain both the hash and the dictionary
 // word.
-char **read_dict(char *filename)
+struct entry **read_dictionary(char *filename, int *size)
 {
+    *size = 0;
     return NULL;
 }
 
@@ -47,15 +46,12 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    // TODO: Read the hash file into an array of strings
-    char **hashes = read_hashes(FILL_THIS_IN);
-
-    // TODO: Read the dictionary file into an array of strings
-    char **dict = read_dict(FILL_THIS_IN);
+    // TODO: Read the dictionary file into an array of entry structures
+    struct entry **dict = read_dictionary(NULL, NULL);
     
     // TODO: Sort the hashed dictionary using qsort.
     // You will need to provide a comparison function.
-    qsort(dict, ___, ___, ___);
+    qsort(dict, 0, 0, NULL);
 
     // TODO
     // For each hash, search for it in the dictionary using
